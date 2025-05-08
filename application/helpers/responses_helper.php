15e5714e6bb8013e7b416a38c0413cf46abf797c
HaqTetsuya <a.r.bayhaqi341@gmail.com>
2025-05-08 20:48:27 +0700

    Update responses_helper.php
    added new intent

--- application/helpers/responses_helper.php
+++ application/helpers/responses_helper.php
@@ -7,78 +7,101 @@ if (!function_exists('get_bot_response')) {
         switch ($intent) {
             case 'greeting':
                 return [
-                    'response' => '<p><strong>Halo!</strong> Ada yang bisa saya bantu hari ini? 😊</p>'
+                    'response' => '<p><strong>Halo, Sir!</strong> ?? Selamat datang di Perpustakaan Digital. Ada yang bisa saya bantu hari ini? ??</p>'
                 ];
 
             case 'goodbye':
                 return [
-                    'response' => '<p><strong>Sampai jumpa!</strong> Semoga harimu menyenangkan! 👋</p>'
+                    'response' => '<p><strong>Sampai jumpa, Sir!</strong> Semoga harimu penuh pengetahuan dan inspirasi. ??</p>'
                 ];
 
             case 'jam_layanan':
                 return [
-                    'response' => '
-                        <strong>Jam Layanan Perpustakaan:</strong><br>
-                        Pelayanan menggunakan sistem tertutup (Closed Access) dengan jam layanan sebagai berikut:<br><br>
-                        <ul>
-                            <li><strong>Senin s.d Kamis</strong>: 08.00 - 15.30</li>
-                            <li><strong>Jumat & Sabtu</strong>: 08.00 - 18.00</li>
-                            <li><strong>Minggu</strong>: 08.00 - 12.00</li>
-                        </ul>'
+                    'response' =>
+                        '<p><strong>?? Jam Layanan Perpustakaan:</strong></p>' .
+                        '<ul>' .
+                        '<li><strong>Senin - Kamis</strong>: 08.00 - 15.30</li>' .
+                        '<li><strong>Jumat & Sabtu</strong>: 08.00 - 18.00</li>' .
+                        '<li><strong>Minggu</strong>: 08.00 - 12.00</li>' .
+                        '</ul>'
                 ];
 
             case 'keanggotaan':
                 return [
-                    'response' => '
-                        <strong>Informasi Keanggotaan:</strong><br>
-                        Untuk menjadi anggota perpustakaan, Anda harus:<br>
-                        <ol>
-                            <li>Mengisi formulir pendaftaran</li>
-                            <li>Melampirkan fotokopi identitas (KTP/KTM)</li>
-                            <li>Menyerahkan pas foto ukuran 3x4 sebanyak 2 lembar</li>
-                        </ol>
-                        <p>Setelah diverifikasi, Anda akan mendapatkan kartu anggota untuk meminjam buku.</p>'
+                    'response' =>
+                        '<p><strong>?? Informasi Keanggotaan:</strong></p>' .
+                        '<ol>' .
+                        '<li>Mengisi formulir pendaftaran online/di tempat</li>' .
+                        '<li>Melampirkan fotokopi KTP/KTM</li>' .
+                        '<li>Menyerahkan 2 lembar pas foto 3x4</li>' .
+                        '</ol>' .
+                        '<p>Setelah data diverifikasi, kartu anggota akan dikirimkan via email/diambil langsung. Kartu ini memungkinkan Anda meminjam buku fisik dan mengakses koleksi digital.</p>'
                 ];
 
             case 'cari_buku':
                 return [
-                    'response' => '
-                        <p><strong>Pencarian Buku:</strong><br>
-                        Silakan ketikkan judul atau pengarang buku yang Anda cari.<br>
-                        <em>Contoh:</em> cari buku pemrograman Python</p>',
+                    'response' =>
+                        '<p><strong>?? Pencarian Buku:</strong><br>' .
+                        'Ketik judul, pengarang, atau kata kunci buku yang Anda cari.<br>' .
+                        '<em>Contoh:</em> cari buku "Pemrograman Python untuk Pemula"</p>',
                     'next_action' => 'wait_book_recommendation'
                 ];
 
-            case 'confirm':			
-				$waitConfirmation = $data['wait_confirmation'] ?? false; 
-				if ($waitConfirmation) {
-					return [
-						'response' => '
-							<p><strong>Pencarian Buku:</strong><br>
-							Silakan ketikkan judul atau pengarang buku yang Anda cari.<br>
-							<em>Contoh:</em> cari buku pemrograman Python</p>',
-						'next_action' => 'confirmation'
-					];
-				} else {
-					return [												
-						'response' => '<p><strong>Baik, terima kasih!</strong> Permintaan Anda telah dikonfirmasi. ?</p>',						
-					];
-				}
-							
-                
+            case 'confirm':
+                $waitConfirmation = $data['wait_confirmation'] ?? false;
+                if ($waitConfirmation) {
+                    return [
+                        'response' =>
+                            '<p><strong>?? Pencarian Buku:</strong><br>' .
+                            'Silakan masukkan kembali judul atau deskripsi buku Anda.</p>',
+                        'next_action' => 'confirmation'
+                    ];
+                } else {
+                    return [
+                        'response' => '<p><strong>?? Terima kasih, Sir!</strong> Permintaan Anda sudah kami proses.</p>'
+                    ];
+                }
+
             case 'denied':
                 return [
-                    'response' => '<p><strong>Permintaan dibatalkan.</strong> Jika butuh bantuan lain, silakan sampaikan. ❌</p>'
+                    'response' => '<p><strong>? Permintaan dibatalkan, Sir.</strong> Silakan sampaikan jika ada yang lain.</p>'
                 ];
-				
-			case 'unknown':
+
+            case 'cara_pinjam':
                 return [
-                    'response' => '<p>Maaf, saya tidak mengerti perintah tersebut.</p>'
+                    'response' =>
+                        '<p><strong>?? Prosedur Peminjaman & Pengembalian Buku:</strong></p>' .
+                        '<p><em>Cara Peminjaman:</em></p>' .
+                        '<ol>' .
+                        '<li>Cari buku yang diinginkan melalui sistem <em>search</em> atau katalog.</li>' .
+                        '<li>Pastikan buku tersedia (status: Tersedia).</li>' .
+                        '<li>Datang ke meja sirkulasi dengan membawa kartu anggota.</li>' .
+                        '<li>Serahkan kartu anggota dan beri tahu petugas judul buku.</li>' .
+                        '<li>Buku akan diproses, kemudian Anda menerima slip peminjaman beserta tanggal kembali.</li>' .
+                        '</ol>' .
+                        '<p><em>Aturan Peminjaman:</em></p>' .
+                        '<ul>' .
+                        '<li>Maksimum 3 buku per anggota.</li>' .
+                        '<li>Durasi peminjaman: 7 hari kerja.</li>' .
+                        '<li>Perpanjangan bisa dilakukan 1 kali jika belum ada pemesan.</li>' .
+                        '</ul>' .
+                        '<p><em>Prosedur Pengembalian:</em></p>' .
+                        '<ol>' .
+                        '<li>Kembalikan buku ke meja sirkulasi atau <em>drop box</em> setelah jam layanan.</li>' .
+                        '<li>Petugas akan memeriksa kondisi buku dan menghapus catatan peminjaman.</li>' .
+                        '<li>Jika terlambat, denda Rp1.000/hari/buku akan dikenakan.</li>' .
+                        '</ol>' .
+                        '<p>Semoga membantu, Sir! ??</p>'
+                ];
+
+            case 'unknown':
+                return [
+                    'response' => '<p>Maaf, Sir, saya belum memahami maksud Anda. Bisa dijelaskan lagi? ??</p>'
                 ];
 
             default:
                 return [
-                    'response' => '<p>Maaf, saya belum bisa memproses permintaan tersebut.</p>'
+                    'response' => '<p>Maaf, Sir, saya belum bisa memproses permintaan tersebut.</p>'
                 ];
         }
     }
